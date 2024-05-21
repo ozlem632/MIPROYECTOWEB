8d0cfc1dbbceb1a37cacc158e6aa49c45fad61e1
ozlem <ozlemasimoglu@gmail.com>
2024-05-21 16:29:42 +0200

    1.commit

---
+++ plataPro/categorias.js
@@ -0,0 +1,643 @@
+var joyas = [
+     {
+            "id": 1,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/YUZUK4.jpeg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 2,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/yuzuk.jpg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 3,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/YUZUK1.jpeg",
+            "categoria": "ANILLOS"
+        },
+    {
+            "id": 4,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/YUZUK2.jpeg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 5,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/YUZUK3.jpeg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 6,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/yuzukemyeni.jpeg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 7,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/YUZUK5.jpeg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 8,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/YUZUK6.jpeg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 9,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/YUZUK7.jpeg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 10,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/YUZUK8.jpeg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 11,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/yuzuka.jpeg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 12,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/yuzukaltinceyrek.jpg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 13,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/yuzukb.jpeg",
+            "categoria": "ANILLOS"
+        },
+     {
+            "id": 14,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 150,
+            "descripcion": "Elegante diseño de estrella de oro.",
+            "imagen": "IMG/yuzuktugrali.jpg",
+            "categoria": "ANILLOS"
+        },
+        
+    
+        {
+            "id": 15,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Estrella",
+            "precio": 200,
+            "descripcion": "Pulsera de oro con diseño de estrella.",
+            "imagen": "IMG/bilezik1.jpeg",
+            "categoria": "PULSERAS"
+        },
+        {
+            "id": 16,
+            "marca": "PLATAPRO",
+            "color": "Plata",
+            "forma": "Mariposa",
+            "precio": 180,
+            "descripcion": "Pulsera de plata con diseño de mariposa.",
+            "imagen": "IMG/bilezik2.jpeg",
+            "categoria": "PULSERAS"
+        },
+        {
+            "id": 17,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bilezikc.jpeg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 18,
+            "marca":"PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bilezikd.jpeg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 19,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bilezikf.jpeg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 20,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bilezikg.jpeg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id":21,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bilezikm.jpeg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 22,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bilezikyeni.jpg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 23,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bileziy.jpg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 24,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bileziks.jpeg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 25,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bilezikenyeni2.jpeg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 26,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bilezikenyeni5.jpeg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 27,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bilezik2.jpeg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 28,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/bilezikenyeni6.jpeg",
+            "categoria": "PULSERAS"
+        }
+    ,
+        {
+            "id": 29,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupe5.jpeg",
+            "categoria": "PENDIENTES"
+        }
+    ,
+        {
+            "id": 30,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupe10.jpeg",
+            "categoria": "PENDIENTES"
+        }
+    ,
+        {
+            "id": 31,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupe11.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 32,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/KUPE12.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 33,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupe13.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 34,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupe14.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 35,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupe15.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 36,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupe17.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 37,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupea.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 38,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupeb.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 40,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupec.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 41,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kuped.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 42,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kupee.jpeg",
+            "categoria":  "PENDIENTES"
+        }
+    ,
+        {
+            "id": 43,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye1.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 44,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye2.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 44,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye10.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 45,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye11.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 46,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye12.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 47,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye13.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 48,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye14.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 49,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye15.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 50,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye16.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 51,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye18.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 52,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolye19.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 53,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolyec.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 54,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolyed.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 55,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolyekanat.jpeg",
+            "categoria": "COLLARES"
+        }
+    ,
+        {
+            "id": 56,
+            "marca": "PLATAPRO",
+            "color": "Oro",
+            "forma": "Círculo",
+            "precio": 220,
+            "descripcion": "Pulsera de oro con diseño de círculo.",
+            "imagen": "IMG/kolyeenyeni.jpeg",
+            "categoria": "COLLARES"
+        }
+];
+
+
+
+
+
+
+
+const baseDeDatos = joyas; 
+
+
+
+
+
+
+
+
+
+
+var categorias = [
+    {
+        "id": 1,
+        "icon": "IMG/YUZUK1.jpeg",
+        "nombre": "ANILLOS"
+    },
+    {
+        "id": 2,
+        "icon": "IMG/bilezikc.jpeg",
+        "nombre": "PULSERAS"
+    },
+    {
+        "id": 3,
+        "icon": "IMG/kolye16.jpeg",
+        "nombre": "COLLARES"
+    },
+    {
+        "id": 4,
+        "icon": "IMG/kolye11.jpeg",
+        "nombre": "PENDIENTES"
+    }
+];
